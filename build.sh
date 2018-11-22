e653cfe306d492baf17ff613a06627682432a688
Matheus Almeida <mat.almeida@live.com>
2018-11-22 14:50:54 -0300

    feat: build script for Windows MacOS and Linux

--- build.sh
+++ build.sh
@@ -24,50 +24,34 @@ if [ "$1" == "--help" ]; then
   exit 0
 fi
 
+BUILD_FOR_ALL="false"
 if [ "$1" == "-a" ]; then
-  if [$# -eq 1]; then
-    echo -e "You need to passa the file that will be built"
-    exit 1
-  fi
   BUILD_FOR_ALL="true"
-  GO_FILE=$2
 fi
 
 if [ "$1" == "--all" ]; then
-  if [$# -eq 1]; then
-    echo -e "You need to passa the file that will be built"
-    exit 1
-  fi
   BUILD_FOR_ALL="true"
-  GO_FILE=$2
 fi
 
 if [ "$2" == "-a" ]; then
-  if [$# -eq 1]; then
-    echo -e "You need to passa the file that will be built"
-    exit 1
-  fi
   BUILD_FOR_ALL="true"
-  GO_FILE=$1
 fi
 
 if [ "$2" == "--all" ]; then
-  if [$# -eq 1]; then
-    echo -e "You need to passa the file that will be built"
-    exit 1
-  fi
   BUILD_FOR_ALL="true"
-  GO_FILE=$1
 fi
-
-echo "Starting Build..."
 
 #Polyfill for MacOsX
 realpath() {
   [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 
-if ["$BUILD_FOR_ALL" == ""]; then
+build_directory="dist"
+directory=$(temp=$( realpath "$build_directory"  ) && dirname "$temp")
+GOBIN=$directory/$build_directory
+
+if [ "$BUILD_FOR_ALL" == "false" ]; then
+  echo "Starting Build..."
   GOARCH=`uname -m`
   case "$GOARCH" in
     i?86) GOARCH="386" ;;
@@ -84,24 +68,103 @@ if ["$BUILD_FOR_ALL" == ""]; then
     *)        GOOS="unknown: $OSTYPE" ;;
   esac
 
-  build_directory="dist/"
-  directory=$(temp=$( realpath "$build_directory"  ) && dirname "$temp")
-  GOBIN=$directory/$build_directory
   if [ -d $GOBIN ]; then
     rm -rf $GOBIN
   fi
   mkdir $GOBIN
 
   echo "Building Server..."
-  env GOOS=$GOOS GOARCH=$GOARCH go build -v server.go
+  env GOOS=$GOOS GOARCH=$GOARCH go build server.go
   echo "Building Client..."
-  env GOOS=$GOOS GOARCH=$GOARCH go build -v client.go
+  env GOOS=$GOOS GOARCH=$GOARCH go build client.go
   mkdir $GOBIN/$OSTYPE
   mv client $GOBIN/$OSTYPE
   mv server $GOBIN/$OSTYPE
   echo "Compressing Files..."
   zip -r $GOBIN/$OSTYPE.zip $GOBIN/$OSTYPE
+  echo "Build Finished."
 fi
 
+if [ "$BUILD_FOR_ALL" == "true" ]; then
+  if [ -d $GOBIN ]; then
+    rm -rf $GOBIN
+  fi
+  mkdir $GOBIN
 
-echo "Build Finished."
+  echo "Build for Windows..."
+  GOOS="windows"
+  echo "\t ix86.."
+  GOARCH="386"
+  echo -e "\t \t Building Server..."
+  env GOOS=$GOOS GOARCH=$GOARCH go build server.go
+  echo -e "\t \t Building Client..."
+  env GOOS=$GOOS GOARCH=$GOARCH go build client.go
+  TARGET_FOLDER=$GOBIN/$GOOS/$GOARCH
+  mkdir -p $TARGET_FOLDER
+  mv client.exe $TARGET_FOLDER
+  mv server.exe $TARGET_FOLDER
+  echo "\t x86_64.."
+  GOARCH="amd64"
+  echo -e "\t \t Building Server..."
+  env GOOS=$GOOS GOARCH=$GOARCH go build server.go
+  echo -e "\t \t Building Client..."
+  env GOOS=$GOOS GOARCH=$GOARCH go build client.go
+  TARGET_FOLDER=$GOBIN/$GOOS/$GOARCH
+  mkdir -p $TARGET_FOLDER
+  mv client.exe $TARGET_FOLDER
+  mv server.exe $TARGET_FOLDER
+  echo "\t Compressing Files..."
+  zip -r $GOBIN/$GOOS.zip $GOBIN/$GOOS
+
+  echo "Build for Linux..."
+  GOOS="linux"
+  echo "\t ix86.."
+  GOARCH="386"
+  echo -e "\t \t Building Server..."
+  env GOOS=$GOOS GOARCH=$GOARCH go build server.go
+  echo -e "\t \t Building Client..."
+  env GOOS=$GOOS GOARCH=$GOARCH go build client.go
+  TARGET_FOLDER=$GOBIN/$GOOS/$GOARCH
+  mkdir -p $TARGET_FOLDER
+  mv client $TARGET_FOLDER
+  mv server $TARGET_FOLDER
+  echo "\t x86_64.."
+  GOARCH="amd64"
+  echo -e "\t \t Building Server..."
+  env GOOS=$GOOS GOARCH=$GOARCH go build server.go
+  echo -e "\t \t Building Client..."
+  env GOOS=$GOOS GOARCH=$GOARCH go build client.go
+  TARGET_FOLDER=$GOBIN/$GOOS/$GOARCH
+  mkdir -p $TARGET_FOLDER
+  mv client $TARGET_FOLDER
+  mv server $TARGET_FOLDER
+  echo "\t Compressing Files..."
+  zip -r $GOBIN/$GOOS.zip $GOBIN/$GOOS
+
+  echo "Build for MacOS..."
+  GOOS="darwin"
+  echo "\t ix86.."
+  GOARCH="386"
+  echo -e "\t \t Building Server..."
+  env GOOS=$GOOS GOARCH=$GOARCH go build server.go
+  echo -e "\t \t Building Client..."
+  env GOOS=$GOOS GOARCH=$GOARCH go build client.go
+  TARGET_FOLDER=$GOBIN/$GOOS/$GOARCH
+  mkdir -p $TARGET_FOLDER
+  mv client $TARGET_FOLDER
+  mv server $TARGET_FOLDER
+  echo "\t x86_64.."
+  GOARCH="amd64"
+  echo -e "\t \t Building Server..."
+  env GOOS=$GOOS GOARCH=$GOARCH go build server.go
+  echo -e "\t \t Building Client..."
+  env GOOS=$GOOS GOARCH=$GOARCH go build client.go
+  TARGET_FOLDER=$GOBIN/$GOOS/$GOARCH
+  mkdir -p $TARGET_FOLDER
+  mv client $TARGET_FOLDER
+  mv server $TARGET_FOLDER
+  echo "\tCompressing Files..."
+  zip -r $GOBIN/$GOOS.zip $GOBIN/$GOOS
+fi
+
+  echo "Build Finished."
